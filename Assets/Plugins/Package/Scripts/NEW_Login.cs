0485d2d59612e8a06631143f6f3f6d8a6dc0381e
Joel <joel.arroyo@asgs.studio>
2024-08-22 15:46:56 -0400

    Updated ASDMEF

--- Assets/Plugins/Package/Scripts/NEW_Login.cs
+++ Assets/Plugins/Package/Scripts/NEW_Login.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NEW_Login : MonoBehaviour
+namespace MaxProFitness.Login
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public class NEW_Login : MonoBehaviour
     {
-        
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
