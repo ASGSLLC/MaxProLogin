fbd0980af3d601fca50ca4fb2c62563c0d335e64
Joel <a2gamedevs@gmail.com>
2024-09-12 11:33:17 -0400

    Added GameOverCanvas
    - NameInput.cs was also added

---
+++ Assets/Plugins/Package/Scripts/UI/EndGame/GameOverCanvas.cs
@@ -0,0 +1,189 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using TMPro;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+using UnityEngine.UI;
+//using Sirenix.OdinInspector;
+
+#if MAXPRO_LOGIN
+using maxprofitness.login;
+#endif
+
+public class GameOverCanvas : YScaleUIBase
+{
+#if AIR_RUNNER
+    public CanvasGroup lockCanvasGroup;
+    public CanvasGroup newBestCanvasGroup;
+
+    public Image levelImage;
+
+    public Sprite[] levelSprits;
+
+    public TextMeshProUGUI levelNameText;
+
+    public TextMeshProUGUI unlockTextLevel;
+    public TextMeshProUGUI unlockTextShip;
+
+    public TextMeshProUGUI yourScoreText;
+    public TextMeshProUGUI bestScoreText;
+    public TextMeshProUGUI ringsCollectedText;
+
+    public CanvasGroupUIBase canvasGroup;
+
+    private GameManager gameManager;
+
+    private ShipController shipController;
+
+    private AirRunnerGameManager airRunnerGameManager;
+
+    private MaxProMobileControlSelection controlSelection;
+
+    private RectTransform rt;
+
+    [SerializeField] private CanvasGroupUIBase modeSelection;
+
+    [SerializeField] private GameObject metricsButton;
+
+    public float startingPosX;
+
+    [Header("Tint")]
+    [SerializeField] private Image tintImage;
+    [SerializeField] private float tintStrength;
+
+    private void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+
+        shipController = FindObjectOfType<ShipController>();
+
+        airRunnerGameManager = FindObjectOfType<AirRunnerGameManager>();
+
+        controlSelection = FindObjectOfType<MaxProMobileControlSelection>();
+
+        HideLevelUnlock();
+
+        canvasGroup = GetComponent<CanvasGroupUIBase>();
+        rt = GetComponent<RectTransform>();
+
+        startingPosX = this.transform.localPosition.x;
+    }
+
+    public void UpdateLevelUnlockText(string _levelText) 
+    {
+        unlockTextLevel.gameObject.SetActive(true);
+        lockCanvasGroup.alpha = 1;
+        unlockTextLevel.text = _levelText;        
+    }
+
+
+    public void UpdateShipUnlockText(string _shipText)
+    {
+        unlockTextShip.gameObject.SetActive(true);
+        lockCanvasGroup.alpha = 1;
+        unlockTextShip.text = _shipText;
+    }
+
+
+    public void HideLevelUnlock() 
+    {
+        unlockTextLevel.gameObject.SetActive(false);
+        unlockTextShip.gameObject.SetActive(false);
+        lockCanvasGroup.alpha = 0;
+    }
+
+
+    public override Tween Show()
+    {
+        if (controlSelection.isUsingMaxProControls == true)
+        {
+            metricsButton.gameObject.SetActive(true);
+        }
+        else
+        {
+            metricsButton.gameObject.SetActive(false);
+        }
+
+        canvasGroup.ForceShow();
+
+        levelNameText.text = AirRunnerGameManager.selectedLevel.levelName;
+
+        switch (AirRunnerGameManager.selectedLevel.levelName)
+        {
+            case "Hills":
+                levelImage.sprite = levelSprits[0];
+                break;
+
+            case "Dark Sky":
+                levelImage.sprite = levelSprits[1];
+                break;
+
+            case "Desert":
+                levelImage.sprite = levelSprits[2];
+                break;
+
+            case "Forest":
+                levelImage.sprite = levelSprits[3];
+                break;
+        }
+        
+        levelImage.preserveAspect = true;
+
+        DOTween.Kill(transform);
+
+        Tween openTween = transform.DOScaleY(1, 0);
+
+        Tween tintTween = tintImage.DOFade(tintStrength, 1);
+
+        tintTween.onComplete = () =>
+        {
+            Tween slideTween = transform.DOLocalMoveX(0, 1);
+        };
+        return tintTween;
+    }
+
+    public void OnMainMenuPressed() 
+    {
+        // Debug.Log("GameOverCanvas//OnRestartPressed//");
+
+        //SceneManager.LoadScene(0);
+
+        airRunnerGameManager.ResetPlayerPosition();
+
+        shipController.resetShipPos();
+
+        //gameManager.StartGame();
+
+        //options.Show(Options.ShowOption.Options, Options.StartLocation.Start);
+
+        transform.localScale = new Vector3(1, 0, 1);
+
+        tintImage.color = Vector4.zero;
+
+        rt.localPosition = new Vector3(startingPosX, 0, 0);
+
+        modeSelection.ForceShow();
+    }
+
+#if UNITY_EDITOR
+    private void OnGUI()
+    {
+        if (transform.localScale.y != 0)
+        {
+            if (GUI.Button(new Rect(10, 70, 50, 30), "Main Menu"))
+            {
+                OnMainMenuPressed();
+            }
+
+            if (GUI.Button(new Rect(10, 120, 50, 30), "ShowGameOver"))
+            {
+                Show();
+            }
+        }
+
+    }
+#endif
+#endif
+}
---
+++ Assets/Plugins/Package/Scripts/UI/EndGame/NameInput.cs
@@ -0,0 +1,171 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+//using UnityEngine.iOS;
+using Firebase.Auth;
+using System.Runtime.CompilerServices;
+#if UNITY_ANDROID
+using UnityEngine.Android;
+#endif
+
+public class NameInput : MonoBehaviour
+{
+    [SerializeField] private bool isUsingProfileName;
+
+    [SerializeField] CanvasGroup NameInputCanvasGroup;
+    [SerializeField] LocalLeaderboard leaderboard;
+
+    [SerializeField] TextMeshProUGUI inputNameText;
+    [SerializeField] TextMeshProUGUI pointValueText;
+
+    [SerializeField] GameOverCanvas airRunnerGameOverCanvas;
+    
+    private TouchScreenKeyboard keyboard;
+    private int currentScore;
+    private int currentGameID;
+
+    //----------------------------------//
+    private void Awake()
+    //----------------------------------//
+    {
+        CloseNameInput();
+
+        if (PlayerPrefs.GetString("InputPlayerName") != null)
+        {
+            //inputNameText.text = PlayerPrefs.GetString("InputPlayerName");
+        }
+    }
+    // END Awake
+
+
+    //----------------------------------//
+    private void Update()
+    //----------------------------------//
+    {
+        if (keyboard != null && keyboard.active == true && inputNameText.text != keyboard.text)
+        {
+            inputNameText.text = keyboard.text; 
+        }        
+    }
+    // END Update
+
+
+    //----------------------------------//
+    public void OpenKeyboard()
+    //----------------------------------//
+    {
+        keyboard = TouchScreenKeyboard.Open("", TouchScreenKeyboardType.Default, false, false, false, false, "Name", 16);
+    }
+    // END OpenKeyboard
+
+    
+
+
+    //----------------------------------//
+    public void GetGameResults(int _score, int _gameID, bool _openNameInput)
+    // Get results from the finished game, should be called when finishing any game before opening the name input canvas
+    //----------------------------------//
+    {
+#if AIR_RUNNER
+        if (isUsingProfileName)
+        {
+            if (UserDataManager.loadedData != null)
+            {
+                // TODO: Replace with firstName with nickName once when leaderboards upload nickname as well
+                inputNameText.text = UserDataManager.loadedData.firstName;
+            }
+
+        }
+        
+        currentScore = _score;
+        currentGameID = _gameID;
+        pointValueText.text = _score.ToString() + " points!";
+        
+        if (_openNameInput)
+        {
+            OpenNameInput();
+        }
+#endif
+    }
+    // END GetGameResults
+
+
+    //----------------------------------//
+    public void OpenNameInput()
+    //----------------------------------//
+    {
+        NameInputCanvasGroup.alpha = 1f;
+        NameInputCanvasGroup.interactable = true;
+        NameInputCanvasGroup.blocksRaycasts = true;
+    }
+    // END OpenNameInput
+
+
+    //----------------------------------//
+    public void CloseNameInput()
+    //----------------------------------//
+    {
+        NameInputCanvasGroup.alpha = 0f;
+        NameInputCanvasGroup.interactable = false;
+        NameInputCanvasGroup.blocksRaycasts = false;
+    }
+    // END CloseNameInput
+
+
+    //----------------------------------//
+    public void SubmitButtonPressed()
+    //----------------------------------//
+    {
+        PlayerPrefs.SetString("InputPlayerName", inputNameText.text);
+        leaderboard.LoadLeaderBoard(leaderboard.gameName, currentGameID);
+        leaderboard.SubmitButton(inputNameText.text, currentScore, currentGameID);
+        EndGame();
+    }
+    // END SubmitButtonPressed
+
+
+    public void EndGame()
+    {
+        // End game depending on current game
+        // 2 = AirRunner Hills || 3 = AirRunner DarkSky || 4 = AirRunner DesertScene || 5 = AirRunner Forest || 6 = RowingCanoeRace || 7 = FitFighter Rhythym
+
+        switch (currentGameID)
+        {
+#if AIR_RUNNER
+            case 2:
+                {
+                    airRunnerGameOverCanvas.OnMainMenuPressed();
+                    break;
+                }
+            case 3:
+                {
+                    airRunnerGameOverCanvas.OnMainMenuPressed();
+                    break;
+                }
+            case 4:
+                {
+                    airRunnerGameOverCanvas.OnMainMenuPressed();
+                    break;
+                }
+            case 5:
+                {
+                    airRunnerGameOverCanvas.OnMainMenuPressed();
+                    break;
+                }
+#endif
+            case 6:
+                {
+                    SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().name);
+                    break;
+                }
+            case 7:
+                {
+                    SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().name);
+                    break;
+                }
+        }
+
+    }
+}
